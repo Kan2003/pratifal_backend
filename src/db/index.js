8c29b8a590b8c9674b11ba4e6cc05c8efa1565ca
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-10-21 12:13:56 +0530

    db update

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 const connectDb = async () => {
     dotenv.config()
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
         console.log(
             `\n MongoDB Connected to ${connection.connection.host}`
           );
