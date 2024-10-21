8c2be3fe112934b3c03f7c9115811dfa2fa942c8
Kan2003 <kanhavishwakarma2003@gmail.com>
2024-10-21 13:03:14 +0530

    package

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { app } from './app.js';
 connectDb()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+      console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => console.error('MongoDB connection failed!', err));
